e831735caf4a4c0e19a5fa0cf98ab89b91c0bffb
anza <cutthenblood@gmail.com>
2013-04-04 14:27:14 +0300

    1

--- DBManager.py
+++ DBManager.py
@@ -11,17 +11,39 @@ class DBManager:
         db = MySQLdb.connect(host=ConnectionsManager.samsonconn['ip'], user=ConnectionsManager.samsonconn['login'],
                              passwd=ConnectionsManager.samsonconn['password'], db=ConnectionsManager.samsonconn['db'], charset='utf8')
         # формируем курсор, с помощью которого можно исполнять SQL-запросы
+
+
         cursor = db.cursor()
-        # исполняем SQL-запрос
+
         cursor.execute(command)
-        # применяем изменения к базе данных
+        while cursor.nextset() is not None: pass
         db.commit()
+
+        # исполняем SQL-запрос
+
+        #cursor.connection.commit()
+        # применяем изменения к базе данных
+        #db.commit()
         id=cursor.lastrowid
         # закрываем соединение с базой данных
         db.close()
         return id
        # engine = create_engine(constr)
        #result=engine.execute(command)
+    @staticmethod
+    def executeproc():
+        db = MySQLdb.connect(host=ConnectionsManager.samsonconn['ip'], user=ConnectionsManager.samsonconn['login'],
+                             passwd=ConnectionsManager.samsonconn['password'], db=ConnectionsManager.samsonconn['db'], charset='utf8',
+                             cursorclass=MySQLdb.cursors.DictCursor)
+        cursor = db.cursor()
+        proc=cursor.callproc( "ImportR23PN_proc", () )
+        result=cursor.fetchall()
+
+        cursor.close()
+        db.close()
+        return result
+
+
 
     @staticmethod
     def executeSql(command):
